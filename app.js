10d7b0ab83b4910e71e1bdb6bc63616d23b8546f
Andre Chris Abad <andreabad17@gmail.com>
2024-01-29 23:46:00 -0800

    It is now reading and writing

--- app.js
+++ app.js
@@ -1,50 +1,57 @@
 // Importing Express
 import express from "express";
 import mongoose from "mongoose";
+
 import dotenv from 'dotenv'
 dotenv.config()
 
+// Model
+const taskSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Please Enter a Task Name"]
+    },
+    priority: {
+        type: Number,
+        required: true,
+        default: 0,
+        
+    },
+})
+
+const Task = mongoose.model('Task', taskSchema);
+
 // Simple Routes
-// const getTask = (req, res) => {
-//     console.log("In Get Task...........")
-//     client.connect((err) => {
-//         if (err) {
-//             res.status(500).send(err)
-//             return
-//         }
-//         const collection = client.db("Task").collection("Tasks");
-//         collection.find().toArray((err, res) => {
-//             if (err) res.status(500).send(err)
-//             if (res) res.json(res)
-//             console.log(res)
-//             client.close()
-//         })
-//     })
-// }
-// const addTask = (req, res) => {
-//     console.log("In Add Task...........")
-//     client.connect((err) => {
-//         if (err) {
-//             res.status(500).send(err)
-//             return
-//         }
-//         const task = req.body
-//         const collection = client.db("Task").collection("Tasks");
-//         collection.insertOne(task, (err, res) => {
-//             if (err) res.status(500).send(err)
-//             if (res) res.json(res)
-//             console.log("Finished.....")
-//             client.close()
-//         })
-//     })
-// }
+const getTask = async (req, res) => {
+    try {
+        const task = await Task.find({});
+        res.status(200).json(task);
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+}
+const addTask = async (req, res) => {
+    console.log(req.body)
+    try {
+        const task = await Task.create(req.body)
+        res.status(200).json(task);
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).json({message: error.message})
+    }
+}
+
+
 
 // New Server Instance
 const app = express()
 
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
 // Routes
-// app.get("/tasks", getTask)
-// app.post('/tasks', addTask)
+app.get("/tasks", getTask)
+app.post('/tasks', addTask)
 
 // Defining the Port
 const PORT = 8008;
@@ -52,8 +59,8 @@ const PORT = 8008;
 mongoose.connect(process.env.URI)
 const db = mongoose.connection
 
-db.on("error",(error)=>{console.error(error)})
-db.once("open",()=>{console.log("Connected")})
+db.on("error", (error) => { console.error(error) })
+db.once("open", () => { console.log("Connected") })
 
 
 app.listen(PORT, () => {
