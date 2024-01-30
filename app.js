fd173bfbf797f763a387d839d77e820101526d71
Andre Chris Abad <andreabad17@gmail.com>
2024-01-29 22:44:18 -0800

    was able to connect the server to the database, going to work on figuring out how to get data now.

--- app.js
+++ app.js
@@ -1,55 +1,72 @@
 // Importing Express
 import express from "express";
-import { client } from "./dbconnect.js"
-require('dotenv').config();
 import mongoose from "mongoose";
+import dotenv from 'dotenv'
+dotenv.config()
 
 // Simple Routes
-const getTask = (req, res) => {
-    console.log("In Get Task...........")
-    client.connect((err) => {
-        if (err) {
-            res.status(500).send(err)
-            return
-        }
-        const collection = client.db("Task").collection("Tasks");
-        collection.find().toArray((err, res) => {
-            if (err) res.status(500).send(err)
-            if (res) res.json(res)
-            console.log(res)
-            client.close()
-        })
-    })
-}
-const addTask = (req, res) => {
-    console.log("In Add Task...........")
-    client.connect((err) => {
-        if (err) {
-            res.status(500).send(err)
-            return
-        }
-        const task = req.body
-        const collection = client.db("Task").collection("Tasks");
-        collection.insertOne(task, (err, res) => {
-            if (err) res.status(500).send(err)
-            if (res) res.json(res)
-            console.log("Finished.....")
-            client.close()
-        })
-    })
-}
+// const getTask = (req, res) => {
+//     console.log("In Get Task...........")
+//     client.connect((err) => {
+//         if (err) {
+//             res.status(500).send(err)
+//             return
+//         }
+//         const collection = client.db("Task").collection("Tasks");
+//         collection.find().toArray((err, res) => {
+//             if (err) res.status(500).send(err)
+//             if (res) res.json(res)
+//             console.log(res)
+//             client.close()
+//         })
+//     })
+// }
+// const addTask = (req, res) => {
+//     console.log("In Add Task...........")
+//     client.connect((err) => {
+//         if (err) {
+//             res.status(500).send(err)
+//             return
+//         }
+//         const task = req.body
+//         const collection = client.db("Task").collection("Tasks");
+//         collection.insertOne(task, (err, res) => {
+//             if (err) res.status(500).send(err)
+//             if (res) res.json(res)
+//             console.log("Finished.....")
+//             client.close()
+//         })
+//     })
+// }
 
 // New Server Instance
 const app = express()
 
 // Routes
-app.get("/tasks", getTask)
-app.post('/tasks', addTask)
+// app.get("/tasks", getTask)
+// app.post('/tasks', addTask)
 
 // Defining the Port
 const PORT = 8008;
+
+mongoose.connect(process.env.URI)
+const db = mongoose.connection
+
+db.on("error",(error)=>{console.error(error)})
+db.once("open",()=>{console.log("Connected")})
 
 
 app.listen(PORT, () => {
     console.log(`Listening on Port ${PORT}`)
 })
+
+// mongoose.
+// connect(process.env.URI)
+// .then(() => {
+//     console.log('connected to MongoDB')
+//     app.listen(`${PORT}`, ()=> {
+//         console.log(`Node API app is running on port 3000`)
+//     });
+// }).catch((error) => {
+//     console.log(error)
+// })
